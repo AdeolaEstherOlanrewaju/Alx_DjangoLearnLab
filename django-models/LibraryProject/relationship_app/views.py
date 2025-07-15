7aef4b7d802b533a656af8ab5387e8191b0429e8
Adeola Olanrewaju <olanrewajuadeola58@gmail.com>
2025-07-15 20:52:54 +0100

    Added views and templates to list books and show library details

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from .models import Library  # Required by the checker
 
 # Function-Based View to list all books
 def list_books(request):
-    books = Book.objects.all()
+    books = Book.objects.all()  # ✅ You must fetch books first
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-Based View to show one library and its books
